f0d43a4cf51e5b9e6c108f58c34127f30d8d836f
Sebastian Pacheco <spacheco-c@3cinteractive.com>
2017-04-23 18:57:08 -0500

    Including the compilation and deployment of the angular app

---
+++ httpd/wait-for-site.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+>&2 echo ">>>>>>>>>> start the heartbreat"
+ping -c 1 $1
+while [  $? -eq 0 ]
+do
+	sleep 1
+	>&2 echo ">>>>>>>>>> $1 running"
+	ping -c 1 $1 
+done
+>&2 echo ">>>>>>>>>> done"
+cp /site/dist/* /var/www/html/
+apachectl -D FOREGROUND
